40a2307f16008b4980097046e8b8961c7fb0324a
liko <lisa.koppe@edu.dsti.institute>
2020-05-13 15:43:43 +0200

    update_05132020

--- 04-Shifted_Schwefels_Problem/Shifted_Schwefel_dim500.py
+++ 04-Shifted_Schwefels_Problem/Shifted_Schwefel_dim500.py
@@ -3,7 +3,7 @@
 # Import all the necessary packages
 import numpy as np
 import pandas as pd
-from scipy.optimize import minimize
+import pygmo as pg
 import matplotlib.pyplot as plt
 import time
 
@@ -20,11 +20,11 @@ D = 500
 bias = -450
 lower_bound = -100
 upper_bound = 100
-sol_list = []
+popsize = 50
 
 
 # Define the Shifted Schwefel's function with the previous parameters
-def function(x, data=schwefel, dim=D, f_bias=bias):
+def function(x, data, dim, f_bias):
     F = abs(x[0] - data[0])
     for i in range(1, dim - 1):
         z = x[i] - data[i]
@@ -33,29 +33,41 @@ def function(x, data=schwefel, dim=D, f_bias=bias):
     return res
 
 
-# Create a function to gather all the solutions computed. To be used in callback
-def sol_set(xk):
-    sol_res = function(xk)
-    sol_list.append(sol_res)
-    return sol_res
+# Create User Defined Problem (UDP) class
+class schwefel_prob:
+    def __init__(self, dim, lower_bound, upper_bound, optim, bias):
+        self.dim = dim
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.optim = optim
+        self.bias = bias
 
+    def fitness(self, x):
+        res = [function(x, self.optim, self.dim, self.bias)]
+        return res
 
-# Create a function to compute the initial guess with random uniform distribution
-def sol_init(dim, lower_bound, upper_bound):
-    xmin = lower_bound * np.ones(dim)
-    xmax = upper_bound * np.ones(dim)
-    x0 = np.random.uniform(min(xmin), max(xmax), dim)
-    return x0
+    def get_bounds(self):
+        xmin = self.lower_bound * np.ones(self.dim)
+        xmax = self.upper_bound * np.ones(self.dim)
+        return xmin, xmax
 
 
 # Create a function to solve this problem
-def solver(dimension, lower_bound, upper_bound):
-    global sol
-    # Compute the initial guess
-    x0 = sol_init(dimension, lower_bound, upper_bound)
-    # Minimize the function thanks to the BFGS algorithm
-    sol = minimize(sol_set, x0, bounds=(lower_bound, upper_bound), method='BFGS', callback=sol_set)
-    return sol, sol_list
+def solver(dimension, lower_bound, upper_bound, optim, bias, popsize):
+    global algo
+    global pop
+    global niter
+    global log
+    global curve
+    prob = pg.problem(schwefel_prob(dimension, lower_bound, upper_bound, optim, bias))
+    algo = pg.algorithm(pg.sga(gen=100000, cr=0.9, eta_c=1.0, m=0.02, param_m=1.0, param_s=2, crossover='sbx', mutation='polynomial', selection='truncated'))
+    algo.set_verbosity(1)
+    pop = pg.population(prob, popsize)
+    pop = algo.evolve(pop)
+    log = algo.extract(pg.sga).get_log()
+    curve = [x[2] for x in log]
+    niter = log[-1][0]
+    return prob, algo, pop, log, niter, curve
 
 
 # Create a function to plot the convergence curve
@@ -65,6 +77,7 @@ def plot_fitness(solution):
     plt.title("Continuous Optimization: Shifted Schwefel's Function (F2) with D=500", fontsize=16)
     plt.xlabel("Time (iterations)", fontsize=12)
     plt.ylabel("Fitness", fontsize=12)
+    plt.legend('SGA', loc='upper right')
     plt.savefig("Screenshots/Schwefel_convergence_curve500.png")
     plt.show()
 
@@ -73,7 +86,7 @@ def plot_fitness(solution):
 t1 = time.time()
 
 # Solve the problem
-solver(D, lower_bound, upper_bound)
+solver(D, lower_bound, upper_bound, schwefel, bias, popsize)
 
 # Stop timer and compute computational time
 t2 = time.time()
@@ -83,20 +96,23 @@ comp_time = t2 - t1
 # Print parameters and solutions
 print("==========================================================================\n")
 print("Function: Shifted Schwefel's Function (F2)\n")
-print("01. Chosen algorithm to solve the problem: BFGS from SciPy\n")
+print("01. Chosen algorithm to solve the problem: SGA from Pygmo\n")
 print("02. Parameters:")
 print("\nDimension:", D)
 print("\nSearch space: [", lower_bound, ",", upper_bound, "]")
 print("\nBias:", bias)
+print("\nPopulation size:", popsize)
+param = algo.get_extra_info()
+print("\n", param)
 print("\n03. Final results:")
-sol_df = pd.DataFrame(sol.x, columns=[''])
+sol_df = pd.DataFrame(pop.champion_x, columns=[''])
 sol_df.to_csv("Schwefel_sol500.csv", sep=",")
 print("\n    - Solutions:", sol_df)
-print("\n    - Fitness:", round(sol.fun, 2))
-print("\nNumber of function evaluations:", sol.nfev)
-print("\nStopping criterion:", sol.nit, "iterations")
+print("\n    - Fitness:", pop.champion_f[0])
+print("\nNumber of function evaluations:", pop.problem.get_fevals())
+print("\nStopping criterion:", niter, "iterations")
 print("\nComputational time:", round(comp_time, 2), "seconds\n")
 print("==========================================================================")
 
 # Plot and save convergence curve
-plot_fitness(sol_list)
+plot_fitness(curve)
