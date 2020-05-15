13ec2460fd6d114db1fdfa5e523cabb66f7587da
liko <lisa.koppe@edu.dsti.institute>
2020-05-15 14:26:59 +0200

    update_05152020

--- 01-TSP_Djibouti/TSP_Djibouti.py
+++ 01-TSP_Djibouti/TSP_Djibouti.py
@@ -4,232 +4,81 @@
 # Import all the necessary packages
 import mlrose
 import pandas as pd
+import matplotlib.pyplot as plt
 import time
 
+# Read data from csv
+cities_raw = pd.read_csv("Data/dj38.csv")
+print(cities_raw)
+print(type(cities_raw))
 
+# Create a matrix of city coordinates from the DataFrame
+cities_list = cities_raw[['x','y']].values.tolist()
+print(cities_list)
+print(type(cities_list))
 
-
-
-
-
-
-
-
-start_time = time.time() # To evaluate computational time
-
-# Download data
-cities = pd.read_csv("E:\DataScience\DSTI\Metaheuristic\Exam\TSP\Djibouti_38.csv")
-
-
-# Create 2D matrix containing x,y coordinate of all the cities
-cities_list = cities[['X','Y']].values.tolist()
+# Initialize function parameters
+nb_cities = int(len(cities_list))
+print(nb_cities)
+population = 5000
 
 # Initialize fitness function object
 fitness_coords = mlrose.TravellingSales(coords=cities_list)
-problem_fit = mlrose.TSPOpt(length = 38, fitness_fn = fitness_coords,
+
+# Define an Optimization Problem Object with TSPOpt()
+problem_fit = mlrose.TSPOpt(length=nb_cities,
+                            fitness_fn=fitness_coords,
                             maximize=False)
 
-# Solve problem using the genetic algorithm
-best_state, best_fitness = mlrose.genetic_alg(problem_fit, random_state = 2)
+# Start timer to get computational time
+t1 = time.time()
 
-print()
-print('The best state found is: ', best_state)
-print()
-print('The fitness at the best state is: ', best_fitness)
-print()
-print("Computational time: {:.3f} seconds".format(time.time()-start_time))
+# Solve the problem using the genetic algorithm
+best_state, best_fitness, fitness_curve = mlrose.genetic_alg(problem=problem_fit,
+                                                             pop_size=population,
+                                                             mutation_prob=0.95,
+                                                             max_attempts=1000,
+                                                             max_iters=10000,
+                                                             curve=True)
 
+# Stop timer and compute computational time
+t2 = time.time()
+comp_time = t2 - t1
 
+# Print parameters and solutions
+print("==========================================================================\n")
+print("Travelling Salesman Problem: Djibouti - 38 cities\n")
+print("01. Chosen algorithm to solve the problem: Genetic Algorithm from mlrose\n")
+print("02. Parameters:")
+print("\nNumber of cities:", nb_cities)
+print("\nPopulation:", population)
+print("\nMutation prob: 0.01")
+print("\nMax attempts: 1000")
+print("\nMax iterations: 10000")
+print("\nNumber of cities:", nb_cities)
+print("\n03. Final results:")
+sol_df = pd.DataFrame(best_state, columns=[''])
+sol_df.to_csv("Djibouti38_sol.csv", sep=",")
+print("\n    - Solutions:", sol_df)
+print("\n    - Fitness:", round(best_fitness, 2))
+print("\nStopping criterion: 1000 iterations")
+print("\nComputational time:", round(comp_time, 2), "seconds\n")
+print("==========================================================================")
 
+# Plot the convergence curve
+fig = plt.figure(figsize=(16, 13))
+plt.plot(-(fitness_curve))
+plt.title("Convergence curve: Travelling Salesman Problem: Djibouti - 38 cities", fontsize=16)
+plt.xlabel("Time (iterations)", fontsize=12)
+plt.ylabel("Fitness", fontsize=12)
+plt.savefig("Screenshots/TSP_convergence_djibouti38.png")
+plt.show()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Import necessary packages
-# import pandas as pd
-# import numpy as np
-# import tsplib95 as tsplib
-# import matplotlib.pyplot as plt
-# import time
-# import mlrose
-# import re
-# import random
-#
-# import math
-# import random
-# import copy
-# import random
-# import matplotlib.pyplot as plt
-# from celluloid import Camera
-#
-#
-# def dist(city1, city2):
-#     return abs(math.sqrt((city1.x-city2.x)**2+(city1.y-city2.y)**2))
-#
-#
-# def eval_all_edges_length(data):
-#     all_edges = []
-#     for i, first in enumerate(data):
-#         all_edges.append([])
-#         for j, second in enumerate(data):
-#             all_edges[i].append(dist(first, second))
-#     return all_edges
-#
-#
-# def total_length(sequence, all_edges):
-#     total = 0
-#     for i, num in enumerate(sequence):
-#         total += all_edges[num][sequence[i - 1]]
-#     return total
-#
-#
-# def find_neighbour(sequence, all_edges):
-#     [item1, item2] = random.sample(sequence, k=2)
-#     index1 = sequence.index(item1)
-#     index2 = sequence.index(item2)
-#     new_sequence = copy.deepcopy(sequence)
-#     new_sequence[index1], new_sequence[index2] = new_sequence[index2], new_sequence[index1]
-#     new_total_distance = total_length(new_sequence, all_edges)
-#     return new_sequence, new_total_distance
-#
-#
-# def plot_graph(data, sequence, plt, camera):
-#     data_x = [_.x for _ in data]
-#     data_y = [_.y for _ in data]
-#     plt.scatter(data_x, data_y, c='#ff0000')
-#     for i, num in enumerate(sequence):
-#         if i is 0:
-#             plt.plot([data[sequence[0]].x, data[sequence[-1]].x], [data[sequence[0]].y, data[sequence[-1]].y])
-#         else:
-#             plt.plot([data[num].x, data[sequence[i - 1]].x], [data[num].y, data[sequence[i - 1]].y])
-#     camera.snap()
-#
-#
-# def acceptance(new, curr, t):
-#     if t is 0:
-#         return 0
-#     else:
-#         return math.exp((curr - new) / t)
-#
-#
-# class Coor:
-#     def __init__(self, index, x, y):
-#         self.index = index
-#         self.x = x
-#         self.y = y
-#
-#
-# def from_file(file_path):
-#     with open(file_path, 'r') as f:
-#         line = f.readline()
-#         output = []
-#         while line:
-#             line = line.replace('\n', '').split(' ')
-#             #line = [float(x) if '.' in x else x for x in line]  # convert decimals
-#             #line = [int(x) if type(x) is str and x.isdigit() else x for x in line]  # convert integers
-#             #if type(line[0]) is int:
-#                 #output.append(Coor(line[0] - 1, line[2], line[1]))
-#             line = f.readline()
-#     return output
-#
-#
-#
-# fig = plt.figure()
-# camera = Camera(fig)
-# data = from_file('../Resources/dj38.tsp')
-# all_edges = eval_all_edges_length(data)
-# sequence = random.sample(range(0, len(data)), len(data))
-# current_total_distance = total_length(sequence, all_edges)
-# list_length = len(sequence)
-#
-# for t in range(500, -1, -1):
-#     for i in range(10000):
-#         new_sequence, new_total_distance = find_neighbour(sequence, all_edges)
-#         if new_total_distance < current_total_distance:
-#             # always accept move when new distance is shorter
-#             sequence = new_sequence
-#             current_total_distance = new_total_distance
-#         elif acceptance(new_total_distance, current_total_distance, t) > random.uniform(0, 1):
-#             # accept move by chance based on acceptance probability e^(delta E / T) where delta E is non-positive
-#             sequence = new_sequence
-#             current_total_distance = new_total_distance
-#     # print data to console and plot graph by step of 100
-#     if t % 10 is 0:
-#         print(f"T: {t}")
-#         print(f'Best Sequence So Far: {sequence}')
-#         print(f'Shortest Distance So Far: {current_total_distance}')
-#         plot_graph(data, sequence, plt, camera)
-#
-# animation = camera.animate()
-# animation.save('wi29.mp4')  # change output video name here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # Set timer
-# start = time.time()
-#
-# # Import TSP data and explore file structure
-# filepath = '../Resources/dj38.tsp'
-# with open(filepath, 'r') as f:
-#     dataset = tsplib.read(f)
-#
-# print(type(dataset))
-# print(dataset.render())
-#
-# # Convert the problem to a networkx graph
-# graph_data = dataset.get_graph()
-# # print(graph_data.nodes)
-# # print(graph_data.graph)
-#
-# print(list(dataset.get_nodes()))
-# print(dataset.node_coords[1])
-#
-#
-# # creating an instance of a TSP problem
-# tsp_instance = tsp(dataset)
-
-
-# if __name__ == "__main__":
-#     main()
-
-# # Create list of city coordinates
-# dict = tsp.models.Problem.as_dict(dataset)
-# print(dict)
-#
-# # Initialize fitness function object using coords_list
-# fitness_coords = mlrose.TravellingSales(coords = dict)
-#
-# problem_fit = mlrose.TSPOpt(length = 38, fitness_fn = fitness_coords, maximize=False)
-
-
-#__________________________________________________________________________________
-
+# Plot route
+fig = plt.figure(figsize=(16, 16))
+plt.plot([cities_list[best_state[i % nb_cities]][0] for i in range(nb_cities+1)], [cities_list[best_state[i % nb_cities]][1] for i in range(nb_cities+1)], 'ro-', linewidth=1)
+plt.title("Best route: Travelling Salesman Problem: Djibouti - 38 cities", fontsize=16)
+plt.xlabel("x coord", fontsize=12)
+plt.ylabel("y coord", fontsize=12)
+plt.savefig("Screenshots/TSP_route_djibouti38.png")
+plt.show()
---
+++ 02-TSP_Qatar/TSP_Qatar.py
@@ -0,0 +1,84 @@
+# Discrete Optimization: Travelling Salesman Problem
+# Qatar - 194 cities
+
+# Import all the necessary packages
+import mlrose
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+
+# Read data from csv
+cities_raw = pd.read_csv("Data/qa194.csv")
+print(cities_raw)
+print(type(cities_raw))
+
+# Create a matrix of city coordinates from the DataFrame
+cities_list = cities_raw[['x','y']].values.tolist()
+print(cities_list)
+print(type(cities_list))
+
+# Initialize function parameters
+nb_cities = int(len(cities_list))
+print(nb_cities)
+population = 5000
+
+# Initialize fitness function object
+fitness_coords = mlrose.TravellingSales(coords=cities_list)
+
+# Define an Optimization Problem Object with TSPOpt()
+problem_fit = mlrose.TSPOpt(length=nb_cities,
+                            fitness_fn=fitness_coords,
+                            maximize=False)
+
+# Start timer to get computational time
+t1 = time.time()
+
+# Solve the problem using the genetic algorithm
+best_state, best_fitness, fitness_curve = mlrose.genetic_alg(problem=problem_fit,
+                                                             pop_size=population,
+                                                             mutation_prob=0.95,
+                                                             max_attempts=1000,
+                                                             max_iters=10000,
+                                                             curve=True)
+
+# Stop timer and compute computational time
+t2 = time.time()
+comp_time = t2 - t1
+
+# Print parameters and solutions
+print("==========================================================================\n")
+print("Travelling Salesman Problem: Qatar - 194 cities\n")
+print("01. Chosen algorithm to solve the problem: Genetic Algorithm from mlrose\n")
+print("02. Parameters:")
+print("\nNumber of cities:", nb_cities)
+print("\nPopulation:", population)
+print("\nMutation prob: 0.01")
+print("\nMax attempts: 1000")
+print("\nMax iterations: 10000")
+print("\nNumber of cities:", nb_cities)
+print("\n03. Final results:")
+sol_df = pd.DataFrame(best_state, columns=[''])
+sol_df.to_csv("Qatar194_sol.csv", sep=",")
+print("\n    - Solutions:", sol_df)
+print("\n    - Fitness:", round(best_fitness, 2))
+print("\nStopping criterion: 1000 iterations")
+print("\nComputational time:", round(comp_time, 2), "seconds\n")
+print("==========================================================================")
+
+# Plot the convergence curve
+fig = plt.figure(figsize=(16, 13))
+plt.plot(-(fitness_curve))
+plt.title("Convergence curve: Travelling Salesman Problem: Qatar - 194 cities", fontsize=16)
+plt.xlabel("Time (iterations)", fontsize=12)
+plt.ylabel("Fitness", fontsize=12)
+plt.savefig("Screenshots/TSP_convergence_qatar194.png")
+plt.show()
+
+# Plot route
+fig = plt.figure(figsize=(16, 16))
+plt.plot([cities_list[best_state[i % nb_cities]][0] for i in range(nb_cities+1)], [cities_list[best_state[i % nb_cities]][1] for i in range(nb_cities+1)], 'ro-', linewidth=1)
+plt.title("Best route: Travelling Salesman Problem: Qatar - 194 cities", fontsize=16)
+plt.xlabel("x coord", fontsize=12)
+plt.ylabel("y coord", fontsize=12)
+plt.savefig("Screenshots/TSP_route_qatar194.png")
+plt.show()
